772c58ad241ef8c1dcc78da470659741b71b9667
Sergey Klyaus <myautneko@gmail.com>
2015-02-22 16:35:01 +0300

    pre- and post-processing

--- src/openproc.py
+++ src/openproc.py
@@ -4,35 +4,36 @@ import re
 import sys
 
 # ----------------------------
-# openproc.py — Collect data from opentrace.py and 
-# join :entry and :return probes 
+# openproc.py - Collect data from opentrace.py and merge :entry and :return probes 
 
-usage = '''openproc.py [filename]'''
-
-def error(msg, *args):
-    print >> sys.stderr, msg % args
-    print >> sys.stderr, usage
-    sys.exit(1)
-
-# Open trace file
+# Open trace file or use stdin 
 try:
     inf = file(sys.argv[1], 'r')
 except OSError as ose:
-    error(str(ose))
+    print ose
+    print '''openproc.py [filename]'''
+    sys.exit(1)
 except IndexError:
     inf = sys.stdin
 
-# Parse /etc/passwd and create UID-to-USERNAME map
-uid_name = lambda user: (int(user[2]), user[0])
-    
-users = [uid_name(user.split(':'))
-         for user in
-         file('/etc/passwd')]
-users = dict(users)
+# Convert time to human time
+def human_time(ns):
+    ns = float(ns)
+    for unit in ['ns', 'us', 'ms']:
+        if abs(ns) < 1000.0:
+            break
+        ns /= 1000.0
+    else:
+        unit = 's'
+    return "%.2f %s" % (ns, unit)
 
-# Per-PID state
-start = {}
-fnames = {}
+# Parse /etc/passwd and create UID-to-USERNAME map
+uid_name = lambda user: (int(user[2]), user[0])    
+users = dict([uid_name(user.split(':'))
+              for user in file('/etc/passwd')])
+
+# Per-PID state - tuples (start time, file name)
+state = {}
 
 # Regular expressions for parsing tracer output
 re_entry = re.compile("=> uid: (\d+) pid: (\d+) open: (.*?) (\d+)")
@@ -44,27 +45,18 @@ for line in inf:
         m = re_entry.match(line)
         _, pid, fname, tm = m.groups()
         
-        pid = int(pid)
-        tm = int(tm)
-        
-        start[pid] = tm
-        fnames[pid] = fname
+        state[int(pid)] = (int(tm), fname)
     elif line.startswith('<='):
         # :return probe, get return value, timestamp and print information
         m = re_return.match(line)
         uid, pid, ret, tm = map(int, m.groups())
         
-        if pid not in start or pid not in fnames:
+        if pid not in state:
             continue
         
-        if ret < 0:
-            status = 'ERROR %d' % ret
-        else:
-            status = 'FD %d' % ret
+        status = 'FD %d' % ret if ret >= 0 else 'ERROR %d' % ret
         
-        print 'OPEN %s %d %s => %s [%d]' % (users.get(uid, str(uid)), 
-                                            pid, fnames[pid], status,
-                                            tm - start[pid])
-        
-        del start[pid]
-        del fnames[pid]
+        print 'OPEN %s %d %s => %s [%s]' % (users.get(uid, str(uid)), 
+                                            pid, state[pid][1], status,
+                                            human_time(tm - state[pid][0]))
+        del state[pid]
--- src/opentrace.py
+++ src/opentrace.py
@@ -1,158 +1,109 @@
 #!/usr/bin/env python
 
-import sys
-import os
-
-import getopt
-import subprocess
+import sys, os, subprocess, platform
+from optparse import OptionParser
 
 # ----------------------------
 # opentrace.py - Trace open syscalls via SystemTap or DTrace
 # supports filtering per UID or PID
 
-usage = '''opentrace.py [-S|-D] [-p PID | -c "COMMAND" | -u UID]
+optparser = OptionParser()
 
--S - Call SystemTap
--D - Call DTrace 
--p - Filter by PID
--u - Filter by UID
--c - Run subcommand and filter by it's pid
+optparser.add_option('-S', '--stap', action='store_true',    
+                     dest='systemtap', help='Run SystemTap')
+optparser.add_option('-D', '--dtrace', action='store_true',   
+                     dest='dtrace', help='Run DTrace')
+optparser.add_option('-p', '--pid', action='store', type='int', 
+                     dest='pid', default='-1', metavar='PID',
+                     help='Trace process with specified PID')
+optparser.add_option('-u', '--uid', action='store', type='int', 
+                     dest='uid', default='-1', metavar='UID', 
+                     help='Filter traced processes by UID')
+optparser.add_option('-c', '--command', action='store', type='string', 
+                     dest='command', metavar='CMD', 
+                     help='Run specified command CMD and trace it')
 
-Environment variables:
-    STAP_PATH - path to SystemTap's binary
-    DTRACE_PATH - path to DTrace's binary
-'''
+(opts, args) = optparser.parse_args()
 
-def error(msg, *args):
-    print >> sys.stderr, msg % args
-    print >> sys.stderr, usage
-    sys.exit(1)
-
-def arg2int(optarg, name):
-    try: 
-        return int(optarg)
-    except TypeError:
-        error('Invalid %s - should be integer', name)
-    
-systemtap   = False
-dtrace      = False
-filemode    = False
-
-pid         = -1
-command     = None
-uid         = -1
-
-try:
-    options, args = getopt.getopt(sys.argv[1:], 'SDp:c:u:')
-except getopt.GetoptError as goe:
-    error(str(goe))
-
-# Process command-line options
-for opt, optarg in options:
-    if opt == '-S':
-        systemtap = True
-    elif opt == '-D':
-        dtrace = True
-    elif opt == '-p':
-        pid = arg2int(optarg, 'PID')
-    elif opt == '-c':
-        command = optarg
-    elif opt == '-u':
-        uid = arg2int(optarg, 'UID')
-    
-    if pid >= 0 and command is not None:
-        error('-p and -c are mutually exclusive')  
-    if (pid >= 0 or command is not None) and uid >= 0:
-        error('-p or -c are mutually exclusive with -u')    
-    if systemtap and dtrace:
-        error('-S and -D are mutually exclusive')
+if opts.pid >= 0 and opts.command is not None:
+    optparser.error('-p and -c are mutually exclusive')  
+if (opts.pid >= 0 or opts.command is not None) and opts.uid >= 0:
+    optparser.error('-p or -c are mutually exclusive with -u')    
+if opts.systemtap and opts.dtrace:
+    optparser.error('-S and -D are mutually exclusive')
             
-if not systemtap and not dtrace:
+if not opts.systemtap and not opts.dtrace:
     # Try to guess based on operating system
-    systemtap = sys.platform == 'linux2'
-    dtrace = sys.platform == 'sunos5'
+    opts.systemtap = sys.platform == 'linux2'
+    opts.dtrace = sys.platform == 'sunos5'
+if not opts.systemtap and not opts.dtrace:
+    optparser.error('DTrace or SystemTap are non-standard for your platform, please specify -S or -D option')
 
-dtrace_probe = '''
-%(name)s 
-/%(cond)s/
-{
-    %(dump)s
-}
-'''
-
-systemtap_probe = '''
-probe %(name)s 
-{
-    if(%(cond)s) next;
-    
-    %(dump)s
-}
-'''
-
-if systemtap:
-    if pid >= 0 or command is not None:
-        cond = 'pid() != target()'
-    elif uid >= 0:
-        cond = 'uid() != %d' % uid
+def run_tracer(entry, ret, cond_proc, cond_user, cond_default, 
+               env_bin_var, env_bin_path, 
+               opt_pid, opt_command, args, fmt_probe):    
+    cmdargs = [os.getenv(env_bin_var, env_bin_path)]
+    if opts.pid >= 0:
+        cmdargs.extend([opt_pid, str(opts.pid)])
+        entry['cond'] = ret['cond'] = cond_proc
+    elif opts.command is not None:
+        cmdargs.extend([opt_command, opts.command])    
+        entry['cond'] = ret['cond'] = cond_proc
+    elif opts.uid >= 0:
+        entry['cond'] = ret['cond'] = cond_user % opts.uid
     else:
-        cond = '0'
+        entry['cond'] = ret['cond'] = cond_default
+    cmdargs.extend(args)
     
+    proc = subprocess.Popen(cmdargs, stdin=subprocess.PIPE)
+    proc.stdin.write(fmt_probe % entry)
+    proc.stdin.write(fmt_probe % ret)
+    
+    proc.stdin.close()    
+    proc.wait()
+
+if opts.systemtap:
     entry = {'name': 'syscall.open',
-             'cond': cond,
              'dump': '''printf("=> uid: %d pid: %d open: %s %d\\n", 
                         uid(), pid(), filename, gettimeofday_ns());'''}
     ret =   {'name': 'syscall.open.return',
-             'cond': cond,
              'dump': '''printf("<= uid: %d pid: %d ret: %d %d\\n", 
                         uid(), pid(), $return, gettimeofday_ns());'''}
     
-    # Build command line arguments
-    cmdargs = [os.getenv('STAP_PATH', '/usr/bin/stap')]
-    if pid >= 0:
-        cmdargs.extend(['-x', str(pid)])
-    elif command is not None:
-        cmdargs.extend(['-c', command])    
-    cmdargs.append('-')
+    run_tracer(entry, ret, cond_proc = 'pid() != target()', 
+               cond_user = 'uid() != %d', cond_default = '0', 
+               env_bin_var = 'STAP_PATH',   
+               env_bin_path = '/usr/bin/stap', 
+               opt_pid = '-x', opt_command = '-c',
+               args = ['-'],
+               fmt_probe = ''' probe %(name)s {
+                    if(%(cond)s) next;
+                    
+                    %(dump)s
+                }
+                ''' )
+elif opts.dtrace:
+    # In Solaris >= 11 open is replaced with openat    
+    is_sol11 = int(platform.release().split('.')[-1]) >= 11
+    sc_name = 'openat*' if is_sol11 else 'open*'
+    fn_arg = 'arg1' if is_sol11 else 'arg0'
     
-    stap = subprocess.Popen(cmdargs, stdin=subprocess.PIPE)
-    stap.stdin.write(systemtap_probe % entry)
-    stap.stdin.write(systemtap_probe % ret)
-    
-    stap.stdin.close()
-    
-    stap.wait()
-elif dtrace:
-    if pid >= 0 or command is not None:
-        cond = 'pid == $target'
-    elif uid >= 0:
-        cond = 'uid == %d' % uid
-    else:
-        cond = '1'
-    
-    entry = {'name': 'syscall::open*:entry',
-             'cond': cond,
-             'dump': '''printf("=> uid: %d pid: %d open: %s %lld\\n", 
-                uid, pid, copyinstr(arg1), (long long) timestamp); '''}
-    ret =   {'name': 'syscall::open*:return',
-             'cond': cond,
-             'dump': '''printf("<= uid: %d pid: %d ret: %d %lld\\n", 
-                        uid, pid, arg1, (long long) timestamp);'''}
+    entry = {'name': 'syscall::%s:entry' % sc_name, 
+        'dump': '''printf("=> uid: %%d pid: %%d open: %%s %%lld\\n", 
+        uid, pid, copyinstr(%s), (long long) timestamp); ''' % fn_arg}
+    ret =   {'name': 'syscall::%s:return' % sc_name,
+        'dump': '''printf("<= uid: %d pid: %d ret: %d %lld\\n", 
+                uid, pid, arg1, (long long) timestamp);'''}
 
-    # Build command line arguments    
-    cmdargs = [os.getenv('DTRACE_PATH', '/usr/sbin/dtrace'), '-q']
-    if pid >= 0:
-        cmdargs.extend(['-p', str(pid)])
-    elif command is not None:
-        cmdargs.extend(['-c', command])    
-    cmdargs.extend(['-s', '/dev/fd/0'])
-    
-    dtrace = subprocess.Popen(cmdargs, stdin=subprocess.PIPE)
-    dtrace.stdin.write(dtrace_probe % entry)
-    dtrace.stdin.write(dtrace_probe % ret)
-    
-    dtrace.stdin.close()
-    
-    dtrace.wait()
-else:
-    error('DTrace or SystemTap are non-standard for your platform, please specify -S or -D option')
-
+    run_tracer(entry, ret, cond_proc = 'pid == $target', 
+               cond_user = 'uid == %d', cond_default = '1', 
+               env_bin_var = 'DTRACE_PATH', 
+               env_bin_path = '/usr/sbin/dtrace', 
+               opt_pid = '-p', opt_command = '-c', 
+               args = ['-q', '-s', '/dev/fd/0'],
+               fmt_probe = ''' %(name)s 
+                /%(cond)s/
+                {
+                    %(dump)s
+                }
+                ''' )
